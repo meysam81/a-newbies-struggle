c78de577f9fb9a10a2fd7661889eabe5e6826734
Meysam Azad <MeysamAzad81@yahoo.com>
2018-01-22 13:09:28 +0330

    initial positioning of the players glv

--- rich-uncle/GlobalVariables.cs
+++ rich-uncle/GlobalVariables.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -8,8 +9,17 @@ namespace rich_uncle
 {
     class GlobalVariables
     {
+        private static short initX = 7, initY = 415;
+        public static Point InitialPoisition
+        {
+            get
+            {
+                initY += 15;
+                return new Point(initX, initY);
+            }
+        }
         // only one player can move at the same time
-        public static Semaphore semaphoreRollTheDice = new Semaphore(1, 1);
+        public static Semaphore initPosLock = new Semaphore(1, 1);
         public static short rollTheDice()
         {
             Random generateRandom = new Random(DateTime.Now.Second);
